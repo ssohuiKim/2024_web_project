7c3107fb9526b7f015f4a76e0343b0cb1409f84b
ssohuiKim <soooooh7@gmail.com>
2024-05-01 15:29:38 +0900

    navbar/pages

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<script>
+
+</script>
+
+
+<style>
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: orange;
+    font-weight: bold; 
+    padding: 1.7em 1em;
+  }
+  
+  nav a {
+    text-decoration: none;
+    color: rgb(255, 255, 255);
+    margin-left: 20px; 
+  }
+  
+  .nav-title a {
+    font-size: 1.5em;
+}
+  .nav-title, .nav-links {
+    display: flex;
+    align-items: center;
+  }
+  
+  .nav-links {
+    justify-content: flex-end;
+    flex-grow: 1;
+  }
+  .nav-links a:last-child {
+  margin-right: 20px; /* 원하는 여백 크기로 조정 */
+}
+
+  .nav-links a:first-child {
+    margin-left: 0;
+  }
+  
+</style>
+
+
+<nav>
+<div class="nav-title">
+  <a href="/" style="margin-right: auto;">STUDY_MOA</a>
+</div>
+<div class="nav-links">
+  <a href="/home">홈</a>
+  <a href="/makemeeting">모임 만들기</a>
+  <a href="/findingstudy">참여하기</a>
+</div>
+</nav>
+
+<slot />
