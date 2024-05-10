ecb7e1b1c4b2f46d2fa8885a75a56a60d22870b4
ssohuiKim <soooooh7@gmail.com>
2024-05-10 16:14:44 +0900

    home_header

--- frontend/src/routes/home/+page.svelte
+++ frontend/src/routes/home/+page.svelte
@@ -1,1 +1,25 @@
-<h1>홈 페이지</h1>
+
+<style>
+.header{
+    text-align: center;
+    background-color: #303030;
+    color: white;
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+  
+.header h1 {
+  font-size: 35px;
+  margin: 0;
+}
+.highlight {
+    color: orange;
+}
+
+</style>
+
+<div class="header">
+    <h1><span class="highlight">STUDY_MOA</span> 에서 함께 공부할 사람 찾기!</h1>
+  </div>
