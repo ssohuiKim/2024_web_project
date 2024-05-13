67d79192a507ec70c2ae17a08d1661ad0d7d2e4a
ssohuiKim <soooooh7@gmail.com>
2024-05-13 23:55:40 +0900

    carousel

--- frontend/src/routes/home/+page.svelte
+++ frontend/src/routes/home/+page.svelte
@@ -1,6 +1,11 @@
+<script>
+	import Carousel from './Carousel.svelte';
+	
+	let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+</script>
 
 <style>
-.header{
+.header {
     text-align: center;
     background-color: #303030;
     color: white;
@@ -8,6 +13,7 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    margin-bottom: 70px;
 }
   
 .header h1 {
@@ -17,9 +23,33 @@
 .highlight {
     color: orange;
 }
-
+.carousel-square {
+		height: 17rem;
+		width: 33rem;
+}
+	
 </style>
 
 <div class="header">
     <h1><span class="highlight">STUDY_MOA</span> 에서 함께 공부할 사람 찾기!</h1>
-  </div>
+</div>
+  
+<Carousel autoplay="2000">
+    {#each colors as color, index (index)}
+      <div class="carousel-square" style="background-color: {color}" />
+    {/each}
+    <span slot="left-control">Left</span>
+    <span slot="right-control">Right</span>
+</Carousel>
+
+  
+<!-- 
+  <Carousel
+    autoplay
+    autoplayDuration={2000}
+  >
+    {#each colors as { color, text } (color)}
+      <Color {color} {text} />
+    {/each}
+  </Carousel>
+   -->
