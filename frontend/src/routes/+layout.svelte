217dd466cf48687d42fd92531e1f5717405e29d4
ssohuiKim <soooooh7@gmail.com>
2024-05-23 17:46:46 +0900

    navbar/footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -5,125 +5,33 @@
 </script>
 
 
-<style>
-nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: orange;
-    font-weight: bold; 
-    padding: 1.7em 1em;
-  }
-  
-  nav a {
-    text-decoration: none;
-    color: rgb(255, 255, 255);
-    margin-left: 20px; 
-  }
-  
-  .nav-title a {
-    font-size: 1.5em;
-}
-  .nav-title, .nav-links {
-    display: flex;
-    align-items: center;
-  }
-  
-  .nav-links {
-    justify-content: flex-end;
-    flex-grow: 1;
-  }
-  .nav-links a:last-child {
-  margin-right: 20px; /* 원하는 여백 크기로 조정 */
-}
-
-  .nav-links a:first-child {
-    margin-left: 0;
-  }
-  
-
-/* 
-  .footer-container {
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-        background-color: #f0f0f0;
-        padding: 20px;
-        align-items: center;
-    }
-
-    .footer-text {
-        color: #333;
-        font-size: 14px;
-        text-align: center;
-    }
-
-    .highlighted {
-        color: orange;
-        margin-right: 8px;
-        margin-left: 25px;
-        font-weight: bold;
-        font-size: 25px;
-    }
-    .left-content {
-        display: flex;
-        align-items: center;
-    }
-
-    .right-content {
-        display: flex;
-        flex-direction: column;
-        margin-top: -35px; 
-    } */
-</style>
 
 
-<!-- <nav>
-<div class="nav-title">
-  <a href="/" style="margin-right: auto;">STUDY_MOA</a>
-</div>
-<div class="nav-links">
-  <a href="/makemeeting">모임 만들기</a>
-  <a href="/findingstudy">참여하기</a>
-</div>
-</nav> -->
-
-
-
-<!-- <div class="footer-container">
-  <div class="left-content">
-      <div class="footer-text highlighted">STUDY MOA</div>
-      <div class="footer-text">
-          | 개인정보 처리방침 | 이용약관
-      </div>
+<Navbar class="max-w-screen-lg mx-auto flex justify-between items-center">
+  <div class="flex items-center">
+    <NavBrand href="/">
+      <span class="self-center whitespace-nowrap text-2xl font-extrabold dark:text-white text-orange-400">STUDY_MOA</span>
+    </NavBrand>
   </div>
-  <div class="right-content">
-      <div class="footer-text">
-          이메일: studymoa_1@000000.com
-      </div>
-      <div class="footer-text">
-          전화번호: 02-000-0000
-      </div>
+  <div class="flex items-center">
+    <NavUl class="flex order-1">
+      <NavLi href="/" active={true}>Home</NavLi>
+      <NavLi href="/makemeeting">모집하기</NavLi>
+      <NavLi href="/findingstudy" class="mr-4">참여하기</NavLi>
+    </NavUl>
+    <div class="flex md:order-2">
+      <Button class="bg-orange-400 text-white hover:bg-orange-600" size="sm">
+        Get started
+      </Button>
+      <NavHamburger />
+    </div>
   </div>
-</div> -->
-<Navbar  >
-  <NavBrand href="/">
-    <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
-  </NavBrand>
-  <NavHamburger  />
-  <NavUl >
-    <NavLi href="/">Home</NavLi>
-    <NavLi href="/about">About</NavLi>
-    <NavLi href="/docs/components/navbar">Navbar</NavLi>
-    <NavLi href="/pricing">Pricing</NavLi>
-    <NavLi href="/contact">Contact</NavLi>
-  </NavUl>
 </Navbar>
 
-<Footer footerType="logo">
+
+<Footer footerType="logo" class="fixed bottom-0 left-0 right-0 mx-auto w-full max-w-screen-lg">
   <div class="sm:flex sm:items-center sm:justify-between">
-    <div class="text-lg font-bold text-gray-900 dark:text-white">STUDY_MOA</div>
+    <span class="text-lg font-bold text-orange-400 text-2xl">STUDY_MOA</span>
     <FooterLinkGroup ulClass="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
       <FooterLink href="/">About</FooterLink>
       <FooterLink href="/">Privacy Policy</FooterLink>
@@ -132,5 +40,5 @@ nav {
     </FooterLinkGroup>
   </div>
   <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-  <FooterCopyright href="/" by=" " />
+  <FooterCopyright href="/" by="STUDY_MOA" class="h-8"/>
 </Footer>
