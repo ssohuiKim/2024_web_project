09169d3bcfaf38e1281b82223b042d945b198026
ssohuiKim <soooooh7@gmail.com>
2024-06-05 17:28:05 +0900

    hero section size

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,38 +1,12 @@
-<!-- <script>
-  import { Img } from 'flowbite-svelte';
+<script>
+
 </script>
 
 <div class="max-w-screen-lg mx-auto h-auto overflow-auto">
-  <Img src="/sample.png" alt="Sample Image" />
-</div> -->
-<script>
-  import { onMount } from 'svelte';
-
-  let showImage = false;
-
-  onMount(() => {
-    showImage = true;
-  });
-</script>
-
-{#if showImage}
-  <div class="max-w-screen-lg mx-auto h-auto overflow-auto">
-    <section class="relative w-full h-screen bg-cover bg-center" style="background-image: url('/sample.png')">
-      <div class="absolute inset-0 bg-black opacity-40"></div>
-      <div class="relative z-10 flex items-center justify-center h-full">
-        <h1 class="text-6xl font-bold text-white">Welcome</h1>
-      </div>
-    </section>
-  </div>
-{/if}
-
-
-
-<!-- <div class="max-w-screen-lg mx-auto h-auto overflow-auto">
-  <section class="relative w-full h-screen bg-cover bg-center" style="background-image: url('/heroImg.avif')">
-    <div class="absolute inset-0 bg-black opacity-50"></div>
+  <section class="relative w-full" style="height: 50vh; background-image: url('/sample.png')">
+    <div class="absolute inset-0 bg-black opacity-40"></div>
     <div class="relative z-10 flex items-center justify-center h-full">
       <h1 class="text-6xl font-bold text-white">Welcome</h1>
     </div>
   </section>
-</div> -->
+</div>
