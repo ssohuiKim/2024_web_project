33f5808eacbdf7481e869b864b0ea30ccc9b55cc
ssohuiKim <soooooh7@gmail.com>
2024-06-05 16:05:34 +0900

    image

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script>
-    import { Button } from 'flowbite-svelte';
-  </script>
+  import { Img } from 'flowbite-svelte';
+</script>
 
 <div class="max-w-screen-lg mx-auto h-auto overflow-auto">
+<Img src="/sample.png" alt="Sample Image" />
+</div>
+
+
+<!-- <div class="max-w-screen-lg mx-auto h-auto overflow-auto">
   <section class="relative w-full h-screen bg-cover bg-center" style="background-image: url('/heroImg.avif')">
     <div class="absolute inset-0 bg-black opacity-50"></div>
     <div class="relative z-10 flex items-center justify-center h-full">
       <h1 class="text-6xl font-bold text-white">Welcome</h1>
     </div>
   </section>
-</div>
+</div> -->
