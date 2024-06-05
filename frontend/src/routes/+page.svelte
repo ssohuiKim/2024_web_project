856b125cc5a50dcf1da3769f2ffe37265d4b13c4
ssohuiKim <soooooh7@gmail.com>
2024-06-05 17:18:28 +0900

    hero section img

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,31 @@
-<script>
+<!-- <script>
   import { Img } from 'flowbite-svelte';
 </script>
 
 <div class="max-w-screen-lg mx-auto h-auto overflow-auto">
-<Img src="/sample.png" alt="Sample Image" />
-</div>
+  <Img src="/sample.png" alt="Sample Image" />
+</div> -->
+<script>
+  import { onMount } from 'svelte';
+
+  let showImage = false;
+
+  onMount(() => {
+    showImage = true;
+  });
+</script>
+
+{#if showImage}
+  <div class="max-w-screen-lg mx-auto h-auto overflow-auto">
+    <section class="relative w-full h-screen bg-cover bg-center" style="background-image: url('/sample.png')">
+      <div class="absolute inset-0 bg-black opacity-40"></div>
+      <div class="relative z-10 flex items-center justify-center h-full">
+        <h1 class="text-6xl font-bold text-white">Welcome</h1>
+      </div>
+    </section>
+  </div>
+{/if}
+
 
 
 <!-- <div class="max-w-screen-lg mx-auto h-auto overflow-auto">
