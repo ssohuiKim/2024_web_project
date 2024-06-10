5ead60e1d5364ae447aec6feee068d5328785dc4
ssohuiKim <soooooh7@gmail.com>
2024-06-10 13:38:19 +0900

    reset

--- backend/myapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import MeetingInfo
-
-class MeetingInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MeetingInfo
-        fields = '__all__'
