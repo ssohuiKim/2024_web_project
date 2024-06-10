6e99d3dd31784d4a8a2a842048d4a813942b4e43
ssohuiKim <soooooh7@gmail.com>
2024-06-10 13:28:44 +0900

    wrong code

--- backend/myproject/meetings/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import MeetingListCreate
-
-urlpatterns = [
-    path('meetings/', MeetingListCreate.as_view(), name='meeting-list-create'),
-]
--- backend/myproject/meetings/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import Meeting
-from .serializers import MeetingSerializer
-
-class MeetingListCreate(generics.ListCreateAPIView):
-    queryset = Meeting.objects.all()
-    serializer_class = MeetingSerializer
