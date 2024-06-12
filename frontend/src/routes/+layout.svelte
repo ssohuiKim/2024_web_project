45339802d1df102d2e439198d4a236ed61012609
ssohuiKim <soooooh7@gmail.com>
2024-06-12 09:02:00 +0900

    modal 생성

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,8 @@
   import "../app.css";
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
   import { Footer, FooterCopyright, FooterLinkGroup, FooterBrand, FooterLink } from 'flowbite-svelte';
+  import { Modal, Label, Checkbox } from 'flowbite-svelte';
+  let formModal = false;
 </script>
 
 
@@ -20,9 +22,7 @@
       <NavLi href="/findingstudy">참여하기</NavLi>
     </NavUl>
     <div class="flex md:order-2">
-      <Button class="bg-orange-400 text-white hover:bg-orange-600" size="sm">
-        Get started
-      </Button>
+      <Button class="bg-orange-400 text-white hover:bg-orange-600" size="sm" on:click={() => (formModal = true)}>Get started</Button>
       <NavHamburger />
     </div>
   </div>
@@ -44,3 +44,26 @@
   <hr class="my-2 sm:mx-auto lg:my-4" />
   <FooterCopyright href="/" by="STUDY_MOA" class="h-4"/>
 </Footer>
+
+
+<Modal bind:open={formModal} size="xs" autoclose={false} class="w-full">
+  <form class="flex flex-col space-y-6" action="#">
+    <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+    <Label class="space-y-2">
+      <span>Email</span>
+      <Input type="email" name="email" placeholder="name@company.com" required />
+    </Label>
+    <Label class="space-y-2">
+      <span>Your password</span>
+      <Input type="password" name="password" placeholder="••••••••" required />
+    </Label>
+    <div class="flex items-start">
+      <Checkbox>Remember me</Checkbox>
+      <a href="/" class="ms-auto text-sm text-primary-700 hover:underline dark:text-primary-500"> Lost password? </a>
+    </div>
+    <Button type="submit" class="w-full1">Login to your account</Button>
+    <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+      Not registered? <a href="/" class="text-primary-700 hover:underline dark:text-primary-500"> Create account </a>
+    </div>
+  </form>
+</Modal>
